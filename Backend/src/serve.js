a774b97d6f53e0eccd287a447e5c3b8122e4afc1
Shubham Umap <shubhamvumap@gmail.com>
2025-06-27 00:39:18 +0530

    add routes

--- Backend/src/serve.js
+++ Backend/src/serve.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +12,9 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
